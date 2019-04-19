7c6c80f172557ee386e32fcc359d333626b1e075
boristim <boris.tim@gmail.com>
2019-04-20 01:13:22 +0500

    Developing

--- sites/all/themes/b3/templates/views/views-view-field--question--page--nothing.tpl.php
+++ sites/all/themes/b3/templates/views/views-view-field--question--page--nothing.tpl.php
@@ -30,12 +30,5 @@ $url = mb_strtolower(explode('/', $_SERVER['HTTP_X_PROTOCOL'])[0]) . '://' . $_S
     <script src="//yastatic.net/share2/share.js"></script>
     <div class="ya-share2" data-url="<?php print $url; ?>" data-title="<?php drupal_get_title(); ?>"
          data-services="vkontakte,facebook,twitter,reddit,lj"></div>
-    <!--  <a href="//vk.com"><img src="/sites/default/files/theme-img/social/vk-white.svg" alt="vk"></a>
-      <a href="//twitter.com"><img src="/sites/default/files/theme-img/social/tw-white.svg" alt="twitter"></a>
-      <a href="//facebook.com"><img src="/sites/default/files/theme-img/social/fb-white.svg" alt="facebook"></a>
-      <a href="//instagram.com"><img src="/sites/default/files/theme-img/social/in-white.svg" alt="twitter"></a>
-      <a href="//livejournal.com"><img src="/sites/default/files/theme-img/social/lj-white.svg" alt="livejournal"></a>
-      <a href="//reddit.com"><img src="/sites/default/files/theme-img/social/rd-white.svg" alt="reddit"></a>
-    -->
   </div>
 </div>
