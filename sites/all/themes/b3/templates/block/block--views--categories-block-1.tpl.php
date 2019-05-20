f28cefe6e5851c694a09950a6d0e99952cfc64cd
boristim <boris.tim@gmail.com>
2019-05-20 19:53:10 +0500

    Developing

--- sites/all/themes/b3/templates/block/block--views--categories-block-1.tpl.php
+++ sites/all/themes/b3/templates/block/block--views--categories-block-1.tpl.php
@@ -51,11 +51,11 @@
 ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php  // print render($title_prefix); ?>
+  <?php  /* print render($title_prefix); ?>
   <?php if ($title): ?>
     <div class="h2 block-titleblock--views--articles-block.tpl.php" <?php print $title_attributes; ?>><?php print $title; ?></div>
   <?php endif;?>
-  <?php // print render($title_suffix);  ?>
+  <?php  print render($title_suffix); */ ?>
 
   <?php print $content ?>
 
