47ad890dfa69d07c8b47299603837771e094e5d1
boristim <boris.tim@gmail.com>
2019-08-23 22:14:11 +0500

    Developing

--- sites/all/themes/b3/templates/system/page.tpl.php
+++ sites/all/themes/b3/templates/system/page.tpl.php
@@ -255,19 +255,28 @@ if (!$show_only_content) {
     <footer class="footer container-fluid">
       <div class="<?php print $container_class; ?> rrr">
         <?php print render($page['footer']); ?>
+
         <div class="footer-counters">
-          <div class="liru"><!--LiveInternet counter-->
-            <script type="text/javascript">
-              document.write("<a href='//www.liveinternet.ru/click' " +
-                "target=_blank><img src='//counter.yadro.ru/hit?t12.11;r" +
-                escape(document.referrer) + ((typeof (screen) == "undefined") ? "" :
-                  ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
-                  screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
-                ";h" + escape(document.title.substring(0, 150)) + ";" + Math.random() +
-                "' alt='' title='LiveInternet: показано число просмотров за 24" +
-                " часа, посетителей за 24 часа и за сегодня' " +
-                "border='0' width='88' height='31'><\/a>")
-            </script><!--/LiveInternet--></div>
+          <div class="liru">
+            <?php
+            if (!$logged_in) {
+              ?>
+              <!--LiveInternet counter-->
+              <script type="text/javascript">
+                document.write("<a href='//www.liveinternet.ru/click' " +
+                  "target=_blank><img src='//counter.yadro.ru/hit?t12.11;r" +
+                  escape(document.referrer) + ((typeof (screen) == "undefined") ? "" :
+                    ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                    screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+                  ";h" + escape(document.title.substring(0, 150)) + ";" + Math.random() +
+                  "' alt='' title='LiveInternet: показано число просмотров за 24" +
+                  " часа, посетителей за 24 часа и за сегодня' " +
+                  "border='0' width='88' height='31'><\/a>")
+              </script><!--/LiveInternet-->
+              <?php
+            }
+            ?>
+          </div>
           <div class="yaru">&nbsp;</div>
           <div class="copyleft"><a href="/node/104"><?php print t('Copyright'); ?></a></div>
           <div class="site-phone"><a href="tel:<?php print variable_get('site_phone'); ?>"><?php print variable_get('site_phone'); ?></a></div>
